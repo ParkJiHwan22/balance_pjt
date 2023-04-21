b5f3d517b65b71769cff91622df062484e574a5b
illson97 <illson97@github.com>
2023-04-21 10:52:30 +0900

    comment

--- posts/urls.py
+++ posts/urls.py
@@ -7,4 +7,13 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('<int:post_pk>', views.detail, name='detail'),
     path('<int:post_pk>/answer/<str:answer>/', views.answer, name='answer'),
+    path('<int:post_pk>/comments/', views.comment_create, name='comment_create'),
+    path(
+        '<int:post_pk>/comments/<int:comment_pk>/delete/',
+        views.comment_delete,
+        name='comment_delete',
+    ),
+    path('<int:post_pk>/likes/', views.likes, name='likes'),
+    path('<int:post_pk>/emotes/<int:emotion>/', views.emotes, name='emotes'),
+    path('<int:post_pk>/comments/<int:comment_pk>/likes/', views.comment_likes, name='comment_likes'),
 ]
